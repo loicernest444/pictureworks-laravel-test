226e80815c2e73461da92beeb9710ce1f8cb4e08
Loïc Ernest <52710467+loicernest444@users.noreply.github.com>
2022-08-01 02:44:20 +0100

    update user comments with artisan command

---
+++ app/Console/Commands/UpdateUserComments.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+class UpdateUserComments extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:comments 
+                                {id: The ID of the user} 
+                                {comments: The new user comment}';
+
+    /**
+     * The console command description. 
+     *
+     * @var string
+     */
+    protected $description = 'Command to update user comments';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        // Retrieve all arguments
+        $id = $this->arguments()["id: The ID of the user"];
+        $comments = $this->arguments()["comments: The new user comment"];
+        
+        // Validate arguments
+        $validator = Validator::make(['id' => $id, 'comments' => $comments], [
+            'id' => 'required|integer|exists:users,id',
+            'comments' => 'required'
+        ]);
+
+        if($validator->fails()){
+            $this->error('Something went wrong!');
+            print_r($validator->errors());
+            return 0;
+        }
+
+        $user = User::find($validator->validated()['id']);
+        
+        $user->updateOrFail([
+            'comments' => $user->comments != '' ? $user->comments . '\n' . $validator->validated()['comments'] : $validator->validated()['comments']
+        ]);
+
+        $this->info('User comments updated sucessfully!');
+        print_r($user);
+        
+        return 0;
+    }
+}
